18ae0f967f4dd88f317f949bc2b8160605cb98b6
MStAr4654 <pilotmanish558@gmail.com>
2025-06-16 12:26:02 +0530

    Edited the 2_functional_test.js file

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,9 +2,17 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
+const { describe, it } = require('mocha');
 
 chai.use(chaiHttp);
-
+/*
 suite('Functional Tests', function() {
 
+});
+*/
+describe('Functional Tests', function () {
+  it('Example Test', function (done) {
+    // Your test logic here
+    done();
+  });
 });
